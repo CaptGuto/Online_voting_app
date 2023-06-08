f5f71d1873dd05cc9933849ef05e5f8b85c3e0e5
CaptGuto <tadenafi@gmail.com>
2023-06-08 08:54:21 +0300

    finished activity_register.xml initiated activity_view_one_time_password.xml/.java

--- app/src/main/java/com/example/online_voting_app/viewOneTimePassword.java
+++ app/src/main/java/com/example/online_voting_app/viewOneTimePassword.java
@@ -3,17 +3,12 @@ package com.example.online_voting_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
-
-public class registerActivity extends AppCompatActivity {
+public class viewOneTimePassword extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_view_one_time_password);
     }
-
-
 }
