e79855db0831ab678204f7572a3055c698f8e162
Bezawit Getu <bgetu90@gmail.com>
2023-06-10 13:26:15 +0300

    Linking the register activity and the viewOTP activity, data is passed from first to second to display on second activity.

--- app/src/main/java/com/example/online_voting_app/registerActivity.java
+++ app/src/main/java/com/example/online_voting_app/registerActivity.java
@@ -2,18 +2,61 @@ package com.example.online_voting_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+import android.view.*;
+import android.widget.*;
+
+import java.security.SecureRandom;
 
 
 public class registerActivity extends AppCompatActivity {
 
+    public Button registerButton;
+    public EditText name, id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
 
+    //Defining method to take input and lead to the next activity also passing the data
+    public void myOnClick (View registerButton) {
+        name = (EditText) findViewById(R.id.name_input_field);
+        id = (EditText) findViewById(R.id.id_input_field);
 
+        //Generating password using method defined below
+        String myPassword = generatePassword(10);
+
+        Intent i = new Intent (registerActivity.this, viewOneTimePassword.class);
+
+        //Passing the name, id, and password to next activity
+        i.putExtra("name", name.getText().toString());
+
+        i.putExtra("ID", id.getText().toString());
+
+        i.putExtra("Password", myPassword);
+
+        startActivity(i);
+
+    }
+
+    private static String generatePassword (int length)
+    {
+        //defining the list of characters the password can contain
+        final String charList = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%";
+
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder();
+
+        //randomly choosing a character by each iteration of the loop and appending to the
+        //StringBuilder instance
+        for (int i = 0; i < length; i++)
+        {
+            int randomIndex = random.nextInt(charList.length());
+            password.append(charList.charAt(randomIndex));
+        }
+
+        return password.toString();
+    }
 }
--- app/src/main/java/com/example/online_voting_app/viewOneTimePassword.java
+++ app/src/main/java/com/example/online_voting_app/viewOneTimePassword.java
@@ -2,7 +2,11 @@ package com.example.online_voting_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.view.View;
+import android.widget.TextView;
+
 import android.os.Bundle;
+import java.security.SecureRandom;
 
 public class viewOneTimePassword extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class viewOneTimePassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_one_time_password);
+
+        //Retrieving user input from the previous activity and assigning them to string references
+        String userName = getIntent().getStringExtra("name");
+        String userID = getIntent().getStringExtra("ID");
+        String OTP = getIntent().getStringExtra("Password");
+
+        //Getting references to the name and id text views
+        TextView nameField = findViewById(R.id.nameOutPutter);
+        TextView IDField = findViewById(R.id.idOutPutter);
+        TextView passwordField = findViewById(R.id.passwordOutPutter);
+
+        //Setting text of text views to the user input values
+        nameField.setText(userName);
+        IDField.setText(userID);
+
+        //Setting text of password view to the generated password
+        passwordField.setText(OTP);
+
     }
+
+
+
+
 }
